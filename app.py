bbe5b99016025c0df7095be1f7941d7897999ebb
Timofey <tim20045@mail.ru>
2022-05-11 14:57:40 +0700

    rus_model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from mediapipe.framework.formats import landmark_pb2
 import mediapipe as mp
 import cv2
 
-model_loaded = keras.models.load_model('gr_rnn01_model')
+model_loaded = keras.models.load_model('gr_rus_rnn_model')#('gr_rnn01_model')
 
 app = Flask(__name__)
 
